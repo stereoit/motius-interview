517f2523f660d77773592a6745eb4f633f4dea6e
Robert Smol <robert.smol@stereoit.com>
2016-10-25 17:58:55 +0200

    Added react-router + simple CSS grid + routes

--- src/app.js
+++ src/app.js
@@ -1,28 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { browserHistory, Router, Route, IndexRoute, Link } from 'react-router'
+
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import UsecaseCard from './components/Usecase';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+import UsecaseCard from './components/UsecaseCard';
+import NoMatch from './components/NoMatch';
+
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-const MainComponent = ({props}) => (
-  <div>
-    Hello from MEAN Stack Test Project
-    <UsecaseCard />
-  </div>
-);
 
-const App = () => (
+const AppLayout = ({children}) => (
   <MuiThemeProvider>
-    <MainComponent />
+    <div>
+      <div className="navigation">
+        <ul>
+          <li><Link to="/">Usecases</Link></li>
+          <li><Link to="/addUsecase">+Add</Link></li>
+        </ul>
+      </div>
+      <div className="page-content">
+        {children}
+      </div>
+    </div>
   </MuiThemeProvider>
 );
 
+const Home = ({props}) => (
+  <div>
+    <h1>Available usecases</h1>
+  </div>
+);
+
+const Usecase = ({props}) => {
+  return (
+    <UsecaseCard />
+  )
+}
+
+const AddUsecase = ({props}) => {
+  return (
+    <div>
+    <h1>Add new usecase</h1>
+    </div>
+  )
+}
+
+
+const routes =
+  <Route path="/" component={AppLayout}>
+    <IndexRoute component={Home} />
+    <Route path="addUsecase" component={AddUsecase} />
+    <Route path="*" component={NoMatch} />
+  </Route>
+
+
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}  routes={routes} />,
   document.getElementById('app')
 );
 
